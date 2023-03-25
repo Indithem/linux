9a64b51f17b03be277c472ab518d308fabd17f4a
Ambarkar Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-03-25 18:10:18 +0530

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,2 +1,2 @@
-cat export http_proxy="http://proxy22.iitd.ac.in:3128" >> ~/.bashrc
-cat export https_proxy="http://proxy22.iitd.ac.in:3128" >> ~/.bashrc
+cat 'export http_proxy="http://proxy22.iitd.ac.in:3128"' >> ~/.bashrc
+cat 'export https_proxy="http://proxy22.iitd.ac.in:3128"' >> ~/.bashrc
