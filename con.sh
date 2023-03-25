0fd350e76b5e09bdf0d40479241c08bc9c75eb65
Ambarkar Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-03-25 18:45:20 +0530

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,2 +1,19 @@
-cat 'export http_proxy="http://proxy22.iitd.ac.in:3128"' >> ~/.bashrc
-cat 'export https_proxy="http://proxy22.iitd.ac.in:3128"' >> ~/.bashrc
+echo export http_proxy="http://proxy22.iitd.ac.in:3128" >> ~/.bashrc
+echo export https_proxy="http://proxy22.iitd.ac.in:3128" >> ~/.bashrc
+
+## Junet installation
+git clone https://github.com/fsquillace/junest.git ~/.local/share/junest
+export PATH=~/.local/share/junest/bin:$PATH
+export PATH="$PATH:~/.junest/usr/bin_wrappers"
+junest setup
+junest proot
+sudo pacman -Sy
+sudo pacman -S fish
+
+## Junet export
+echo export PATH=~/.local/share/junest/bin:$PATH >> ~/.bashrc
+echo export PATH="$PATH:~/.junest/usr/bin_wrappers" >> ~/.bashrc
+echo fish >> ~/.bashrc
+
+echo Restart your terminal
+
