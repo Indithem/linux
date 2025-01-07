ba64cb9b78e2ae216707635c51f53f587776e6b4
Sai Srinivas <cs1221620@cse.iitd.ac.in>
2025-01-08 01:14:34 +0530

    use wget to download instead of git

--- junest.sh
+++
@@ -1,4 +0,0 @@
-sudo pacman -Sy
-sudo pacman -S  nano python3 fish zip git
-echo fish >> ~/.bashrc
-exit
--- setup_junest.sh
+++ setup_junest.sh
@@ -1,6 +1,7 @@
 JUNEST_SRC_DIR=/tmp/local/junest_src
-JUNEST_DIR=/tmp/local/junest
+JUNEST_HOME=/tmp/local/junest
 
+set -e
 mkdir -p $JUNEST_SRC_DIR $JUNEST_DIR
 
 # ensure running with source ...
@@ -10,14 +11,26 @@ echo "Dont run directly. source this file."
 exit 1
 fi
 
+JUNEST_VERSION=7.4.10
 
-export JUNEST_HOME=$JUNEST_DIR
-export PATH=~/.junest/usr/bin_wrappers:$JUNEST_SRC_DIR/bin:$PATH
+wget https://github.com/fsquillace/junest/archive/refs/tags/$JUNEST_VERSION.zip -O $JUNEST_SRC_DIR/.zip
+unzip -o $JUNEST_SRC_DIR/.zip -d $JUNEST_SRC_DIR
 
-git clone --depth 1 --branch 7.4.10 https://github.com/fsquillace/junest.git $JUNEST_SRC_DIR
+export JUNEST_HOME=$JUNEST_HOME
+export PATH=~/.junest/usr/bin_wrappers:$JUNEST_SRC_DIR/junest-$JUNEST_VERSION/bin:$PATH
 
+if [ -z "$( ls -A $JUNEST_HOME )" ]; then
+# create new root dir
 junest setup
+fi
+
+# install packages for user
+junest sudo pacman -Syu --noconfirm neovim fish zip git
+
+alias jj='junest fish'
 
 
-echo "Now run 'junest' to enter your proot"
+echo "Now you can run 'junest' to enter proot"
+echo "Use alias 'jj' to open fish directly."
+
 
